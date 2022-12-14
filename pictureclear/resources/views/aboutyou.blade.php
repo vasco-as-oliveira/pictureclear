f4bdca5095d46a9c26ecb97ef612f83309a42fa0
Jaime Costa <tmdcosta.17@gmail.com>
2022-12-14 14:15:59 +0000

    Css completo, falta o route

--- pictureclear/resources/views/aboutyou.blade.php
+++ pictureclear/resources/views/aboutyou.blade.php
@@ -1,24 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <link rel="stylesheet" href="css/styleAboutYou.css">
-    <div class="container-aboutyou" id="container-aboutyou">
-        <div class="form-container about-container">
-            <form method="POST" class="aboutyou">
+    <link rel='stylesheet' href='css/styleAboutYou.css' />
+    <div class="container-login">
+        <div class="form-container sign-in-container">
+            <form method="POST" class="login" action="{{ route('login') }}">
                 @csrf
-                <div class="aboutyou">
-                    <h1>Conta-nos mais sobre ti!</h1>
-                    <div class="submit">
-                        <input type="text">
-                        <button type="submit">Concluído</button>
-                    </div>
-                    <div class="profilepicture">
-                        <img src="images/default-profilepicture.png" alt="default-profilepicture" id="profilepicture">
-                        <input type="file" id="profilepictureInput">
-                        <label for="profilepictureInput" id="upload">Escolhe uma foto!</label>
-                    </div>
+                <h1>Conta-nos mais sobre ti {{ Auth::user()->firstname}}!</h1>
+                <div class="submit">
+                    <textarea name="description" class="text" placeholder="Eu sou @ melhor!"></textarea>
+                    <button name="done" type="submit">Concluído</button>
                 </div>
             </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-right">
+                    <div class="profilepicture">
+                        <h1 id="photo">Wow!</h1>
+                        <br>
+                        <img src="images/default-profilepicture.png" alt="default-profilepicture" id="profilepicture">
+                        <input type="file" id="profilepictureInput" style="display: none">
+                        <button class="ghost">
+                            <label for="profilepictureInput" id="upload">Escolhe uma foto!</label>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <script>
@@ -26,14 +34,7 @@
         const img_profilepicture = document.querySelector('#profilepicture');
         const input_profilepictureInput = document.querySelector('#profilepictureInput');
         const button_upload = document.querySelector('#upload');
-
-        div_pfp.addEventListener('mouseenter', function() {
-            button_upload.style.display = "block";
-        });
-
-        div_pfp.addEventListener('mouseleave', function() {
-            button_upload.style.display = "none";
-        });
+        const h1_photo = document.querySelector('#photo');
 
         input_profilepictureInput.addEventListener('change', function() {
             const chosenPhoto = this.files[0];
@@ -43,6 +44,7 @@
                     img_profilepicture.setAttribute('src', reader.result);
                 });
                 reader.readAsDataURL(chosenPhoto);
+                h1_photo.style.display="contents";
             }
         });
     </script>
