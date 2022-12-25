89d20b0e8b02aca2695335d3e6cce81d770fbf71
vasco-as-oliveira <118202460+vasco-as-oliveira@users.noreply.github.com>
2022-12-25 14:23:15 +0000

    Config changes

--- pictureclear/config/app.php
+++ pictureclear/config/app.php
@@ -181,6 +181,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        ProtoneMedia\LaravelFFMpeg\Support\ServiceProvider::class,
+
 
         /*
          * Package Service Providers...
@@ -209,6 +211,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'FFMpeg' => ProtoneMedia\LaravelFFMpeg\Support\FFMpeg::class
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
