08ad012028cec56debed97913bd15a39175e9890
Jaime Costa <tmdcosta.17@gmail.com>
2023-01-14 14:53:36 +0000

    eloquent

--- pictureclear/app/Http/Controllers/BuyController.php
+++ pictureclear/app/Http/Controllers/BuyController.php
@@ -29,8 +29,7 @@ class BuyController extends Controller
     }
 
     public function index(Request $request)
-    {   
-        //$subscribed_usersTest = DB::select('select user_id from sales where tier_id IN(select id from tiers where course_id=' . $request->course . ') and user_id=' . Auth::User()->id . '');
+    {
         $arrayOfTiers = Tier::select('id')
                         ->where('course_id', '=', $request->course)
                         ->where('user_id', '=', Auth::User()->id);
@@ -38,18 +37,17 @@ class BuyController extends Controller
         if (count($subscribed_users)>0){
             return back();
         }
-        //$course = DB::select("SELECT * from courses where id =". $request->course);
+
         $course = Course::select('*')
                         ->where('id', '=', $request->course)->get()->toArray();
-        //$tiers = DB::select("SELECT * from tiers where course_id =". $request->course);
+
         $tiers = Tier::select('*')
                     ->where('course_id', '=', $request->course)->get()->toArray();
         return view("buyCourse", ["course"=>$course, "tiers" => $tiers]);
     }
 
-    public function buy(Request $request){
-        
-        //$price = DB::select("SELECT price from tiers where id =". $request->tier);
+    public function buy(Request $request)
+    {
         $price = Tier::select('price')
                         ->where('id', '=', $request->tier)->get()->toArray();
         if ($request->saldo){
